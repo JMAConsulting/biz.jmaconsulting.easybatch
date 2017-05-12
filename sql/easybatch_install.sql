42587c46970f6673b9bdd5e01f902e69efa9fa95
Pradeep Nayak <pradpnayak@gmail.com>
2017-05-12 17:24:02 +0530

    IIDA-95, fixed install code

--- sql/easybatch_install.sql
+++ sql/easybatch_install.sql
@@ -2,11 +2,17 @@
 -- Table structure for table `civicrm_easybatch_entity`
 --
 
-CREATE TABLE IF NOT EXISTS `civicrm_easybatch_entity` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `batch_id` int(10) NOT NULL,
-  `contact_id` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-ALTER TABLE `civicrm_easybatch_entity` ADD CONSTRAINT `civicrm_batch_id_contact_id` UNIQUE (`batch_id`, `contact_id`);
+CREATE TABLE  IF NOT EXISTS `civicrm_easybatch_entity` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT  ,
+  `batch_id` int unsigned NOT NULL   COMMENT 'FK to Batch ID',
+  `contact_id` int unsigned NOT NULL   COMMENT 'FA organization id',
+  `payment_processor_id` int unsigned    COMMENT 'FK payment processor id',
+  `is_automatic` tinyint NOT NULL  DEFAULT 0,
+  PRIMARY KEY (`id`),
+  CONSTRAINT FK_civicrm_easybatch_entity_batch_id FOREIGN KEY (`batch_id`)
+    REFERENCES `civicrm_batch`(`id`) ON DELETE CASCADE,
+  CONSTRAINT FK_civicrm_easybatch_entity_contact_id FOREIGN KEY (`contact_id`)
+    REFERENCES `civicrm_contact`(`id`) ON DELETE CASCADE,
+  CONSTRAINT FK_civicrm_easybatch_entity_payment_processor_id FOREIGN KEY (`payment_processor_id`)
+    REFERENCES `civicrm_payment_processor`(`id`) ON DELETE CASCADE  
+)  ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
